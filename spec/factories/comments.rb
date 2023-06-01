aa5894b94d83138847b3628099756423c565be18
Steven Chan <stevenchan@oursky.com>
2023-06-01 17:08:42 +0100

    Add spec for comment validation

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :comment do
-    
+    content { Faker::Lorem.paragraph(sentence_count: 2) }
+
+    association :created_by, factory: :user
+    association :updated_by, factory: :user
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:project) { FactoryBot.create(:project) }
+  let(:comment) { FactoryBot.create(:comment, project: project) }
+
+  it "content cannot be empty" do
+    comment.content = ""
+    expect(comment.save).to be(false)
+  end
+
+  it "content cannot be nil" do
+    comment.content = nil
+    expect(comment.save).to be(false)
+  end
 end
