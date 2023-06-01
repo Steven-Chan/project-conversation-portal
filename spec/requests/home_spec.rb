e00a9c11e286a1ff3c1fef5cf9aa27cd92fa2533
Steven Chan <stevenchan@oursky.com>
2023-06-01 11:25:10 +0100

    Add simple request test for home page

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let!(:user) { FactoryBot.create(:user) }
+
+  describe "GET /" do
+    it "show home page successfully" do
+      get '/'
+      expect(response.status).to eq(200)
+    end
+
+    it "show home page successfully after login" do
+      sign_in_as(user)
+      get '/'
+      expect(response.status).to eq(200)
+    end
   end
 end
---
+++ spec/support/auth_helper.rb
@@ -0,0 +1,11 @@
+module AuthHelper
+  def sign_in_as(user)
+    post user_session_path, params: {
+      user: {
+        email: user.email, password: user.password
+      }
+    }
+
+    follow_redirect!
+  end
+end
